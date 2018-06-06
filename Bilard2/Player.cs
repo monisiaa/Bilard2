4ed64e4ce153eb701a46d66e6618d1ebb95a442c
weronikawierzbicka <weronikawierzbicka@op.pl>
2018-06-06 12:48:53 +0200

    Uzupełnienie klasy Player

--- Bilard2/Player.cs
+++ Bilard2/Player.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Bilard2
 {
-    class Player
+    public class Player
     {
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
     }
 }
