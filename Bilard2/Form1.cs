7cd9b3ba9b8f9f14cd6f15871c0032aade861dfc
weronikawierzbicka <weronikawierzbicka@op.pl>
2018-07-13 20:15:38 +0200

    Uaktualnienie

--- Bilard2/Form1.cs
+++ Bilard2/Form1.cs
@@ -47,13 +47,16 @@ namespace Bilard2
             SolidBrush lightgreenBrush = new SolidBrush(Color.LawnGreen);
             SolidBrush brownBrush = new SolidBrush(Color.DarkRed);
             SolidBrush lightbrownBrush = new SolidBrush(Color.SaddleBrown);
-#endregion
+            #endregion
             Rectangle table = new Rectangle(35, 35, 625, 325);
 
             Graphics t = CreateGraphics();
             Graphics r = CreateGraphics();
             t.FillRectangle(greenBrush, table);
             r.DrawRectangle(grayPen, 35, 35, 635, 335);
+
+            sb.fromTheWalls();
+
             #region[łuzy]
             Graphics l1 = CreateGraphics();
             l1.FillEllipse(blackBrush, 36, 36, 35, 35);
@@ -68,87 +71,105 @@ namespace Bilard2
             Graphics l6 = CreateGraphics();
             l6.FillEllipse(blackBrush, 36, 335, 35, 35);
             #endregion
+
             #region[bile]
-            int x_w = 550, y_w = 190, vx_w=0, vy_w=0;
+            int x_w = 550, y_w = 190, vx_w = 0, vy_w = 0;
             Ball white = sb.addBall(x_w, y_w, vx_w, vy_w);
+           // sb.balls.Add(white);
             Graphics WhiteBall = CreateGraphics();
             WhiteBall.FillEllipse(whiteBrush, white.X, white.Y, white.R, white.R);
 
             int x_1 = 164, y_1 = 216, vx_1 = 0, vy_1 = 0;
             Ball yellow = sb.addBall(x_1, y_1, vx_1, vy_1);
+            sb.balls.Add(yellow);
             Graphics YellowBall = CreateGraphics();
             YellowBall.FillEllipse(yellowBrush, yellow.X, yellow.Y, yellow.R, yellow.R);
 
             int x_2 = 120, y_2 = 164, vx_2 = 0, vy_2 = 0;
             Ball blue = sb.addBall(x_2, y_2, vx_2, vy_2);
+            sb.balls.Add(blue);
             Graphics BlueBall = CreateGraphics();
             BlueBall.FillEllipse(blueBrush, blue.X, blue.Y, blue.R, blue.R);
 
             int x_3 = 142, y_3 = 203, vx_3 = 0, vy_3 = 0;
             Ball red = sb.addBall(x_3, y_3, vx_3, vy_3);
+            sb.balls.Add(red);
             Graphics RedBall = CreateGraphics();
             RedBall.FillEllipse(redBrush, red.X, red.Y, red.R, red.R);
 
             int x_4 = 120, y_4 = 216, vx_4 = 0, vy_4 = 0;
             Ball purple = sb.addBall(x_4, y_4, vx_4, vy_4);
+            sb.balls.Add(purple);
             Graphics PurpleBall = CreateGraphics();
             PurpleBall.FillEllipse(purpleBrush, purple.X, purple.Y, purple.R, purple.R);
 
             int x_5 = 120, y_5 = 242, vx_5 = 0, vy_5 = 0;
             Ball orange = sb.addBall(x_5, y_5, vx_5, vy_5);
+            sb.balls.Add(orange);
             Graphics OrangeBall = CreateGraphics();
             OrangeBall.FillEllipse(orangeBrush, orange.X, orange.Y, orange.R, orange.R);
 
             int x_6 = 142, y_6 = 151, vx_6 = 0, vy_6 = 0;
             Ball darkgreen = sb.addBall(x_6, y_6, vx_6, vy_6);
+            sb.balls.Add(darkgreen);
             Graphics DarkGreenBall = CreateGraphics();
             DarkGreenBall.FillEllipse(darkgreenBrush, darkgreen.X, darkgreen.Y, darkgreen.R, darkgreen.R);
 
             int x_7 = 186, y_7 = 177, vx_7 = 0, vy_7 = 0;
             Ball brown = sb.addBall(x_7, y_7, vx_7, vy_7);
+            sb.balls.Add(brown);
             Graphics BrownBall = CreateGraphics();
             BrownBall.FillEllipse(brownBrush, brown.X, brown.Y, brown.R, brown.R);
 
             int x_8 = 164, y_8 = 190, vx_8 = 0, vy_8 = 0;
             Ball black = sb.addBall(x_8, y_8, vx_8, vy_8);
+            sb.balls.Add(black);
             Graphics BlackBall = CreateGraphics();
             BlackBall.FillEllipse(blackBrush, black.X, black.Y, black.R, black.R);
 
             int x_9 = 208, y_9 = 190, vx_9 = 0, vy_9 = 0;
             Ball lightyellow = sb.addBall(x_9, y_9, vx_9, vy_9);
+            sb.balls.Add(lightyellow);
             Graphics LightYellowBall = CreateGraphics();
             LightYellowBall.FillEllipse(lightyellowBrush, lightyellow.X, lightyellow.Y, lightyellow.R, lightyellow.R);
 
             int x_10 = 142, y_10 = 177, vx_10 = 0, vy_10 = 0;
             Ball lightblue = sb.addBall(x_10, y_10, vx_10, vy_10);
+            sb.balls.Add(lightblue);
             Graphics LightBlueBall = CreateGraphics();
             LightBlueBall.FillEllipse(lightblueBrush, lightblue.X, lightblue.Y, lightblue.R, lightblue.R);
 
             int x_11 = 120, y_11 = 138, vx_11 = 0, vy_11 = 0;
             Ball lightred = sb.addBall(x_11, y_11, vx_11, vy_11);
+            sb.balls.Add(lightred);
             Graphics LightRedBall = CreateGraphics();
             LightRedBall.FillEllipse(lightredBrush, lightred.X, lightred.Y, lightred.R, lightred.R);
 
             int x_12 = 186, y_12 = 203, vx_12 = 0, vy_12 = 0;
             Ball lightpurple = sb.addBall(x_12, y_12, vx_12, vy_12);
+            sb.balls.Add(lightpurple);
             Graphics LightPurpleBall = CreateGraphics();
             LightPurpleBall.FillEllipse(lightpurpleBrush, lightpurple.X, lightpurple.Y, lightpurple.R, lightpurple.R);
 
             int x_13 = 120, y_13 = 190, vx_13 = 0, vy_13 = 0;
             Ball lightorange = sb.addBall(x_13, y_13, vx_13, vy_13);
+            sb.balls.Add(lightorange);
             Graphics LightOrangeBall = CreateGraphics();
             LightOrangeBall.FillEllipse(lightorangeBrush, lightorange.X, lightorange.Y, lightorange.R, lightorange.R);
 
             int x_14 = 142, y_14 = 229, vx_14 = 0, vy_14 = 0;
             Ball lightgreen = sb.addBall(x_14, y_14, vx_14, vy_14);
+            sb.balls.Add(lightgreen);
             Graphics LightGreenBall = CreateGraphics();
             LightGreenBall.FillEllipse(lightgreenBrush, lightgreen.X, lightgreen.Y, lightgreen.R, lightgreen.R);
 
             int x_15 = 164, y_15 = 164, vx_15 = 0, vy_15 = 0;
             Ball lightbrown = sb.addBall(x_15, y_15, vx_15, vy_15);
+            sb.balls.Add(lightbrown);
             Graphics LightBrownBall = CreateGraphics();
             LightBrownBall.FillEllipse(lightbrownBrush, lightbrown.X, lightbrown.Y, lightbrown.R, lightbrown.R);
             #endregion
+
 
             Player player1 = player.addPlayer(px1, py1, px2, py2);
 
--- Bilard2/SimulationBox.cs
+++ Bilard2/SimulationBox.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows;
 
 namespace Bilard2
 {
     public class SimulationBox
     {
+
         public const int radius = 26;
+
+        List<Ball> ballList = new List<Ball>();
 
         public Ball addBall(int X, int Y, int VX, int VY)
         {
@@ -24,8 +28,81 @@ namespace Bilard2
                 R = radius,
                 Mass = 1
             };
+            ballList.Add(b);
             return b;
         }
-        
+
+        public void fromTheWalls()
+        {
+            foreach (var b in ballList)
+            {
+                if (b.X < 0 || b.X + b.D > 625)
+                {
+                    b.VX = -b.VX;
+                }
+                if (b.Y < 0 || b.Y + b.D > 325)
+                {
+                    b.VY = -b.VY;
+                }
+
+                b.X = b.X + b.VX;
+                b.Y = b.Y + b.VY;
+
+            }
+        }
+
+       /* public void Collission()
+        {
+            for (int i = 0; i < ballList.Count(); i++)
+            {
+                for (int j = i + 1; j < ballList.Count(); j++)
+                {
+                    var b1 = ballList[i];
+                    var b2 = ballList[j];
+
+                    if (Math.Sqrt(Math.Pow(b1.X - b2.X, 2) + Math.Pow(b1.Y - b2.Y, 2)) - (b1.R + b2.R) < 0)
+                    {
+
+                        // http://www.gamasutra.com/view/feature/131424/pool_hall_lessons_fast_accurate_.php?page=3
+
+                        Vector pos1 = new Vector(b1.X, b1.Y);
+                        Vector pos2 = new Vector(b2.X, b2.Y);
+
+                        Vector N = pos1 - pos2;
+                        N.Normalize();
+
+                        Vector v1 = new Vector(b1.VX, b1.VY);
+                        Vector v2 = new Vector(b2.VX, b2.VY);
+
+                        // a1 =  v1 . N
+                        double a1 = Vector.Multiply(v1, N);
+
+                        // a2 = v2 . N
+                        double a2 = Vector.Multiply(v2, N);
+
+                        double m1 = b1.Mass;
+                        double m2 = b2.Mass;
+
+                        double p = 2 * (a1 - a2) / (m1 + m2);
+
+                        Vector newV1 = v1 - p * m2 * N;
+                        Vector newV2 = v2 + p * m1 * N;
+
+                        b1.VX = newV1.X;
+                        b1.VY = newV1.Y;
+                        b2.VX = newV2.X;
+                        b2.VY = newV2.Y;
+                    }
+                }
+            }
+        }
+        */
+        public List<Ball> balls
+        {
+            get
+            {
+                return ballList;
+            }
+        }
     }
 }
